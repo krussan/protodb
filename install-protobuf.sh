0ad844ad187efcf445175684c16fd618297f17f6
Christian Thunström <christian@qxx.se>
2017-03-04 21:42:15 +0100

    Removed local bin from pom file and amended travis build file

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -2,6 +2,6 @@
 set -ex
 wget https://github.com/google/protobuf/releases/download/v2.4.1/protobuf-2.4.1.tar.gz
 tar -xzvf protobuf-2.4.1.tar.gz
-cd protobuf-2.4.1 && ./configure && make && sudo make install
+cd protobuf-2.4.1 && ./configure --prefix=/usr && make && sudo make install
 
 
