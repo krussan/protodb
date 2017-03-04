ebf286759f7780f194e15f3f1fe8bf0b3cde3153
Christian Thunström <christian@qxx.se>
2017-03-04 21:16:03 +0100

    Added protobuf install to travis build

---
+++ install-protobuf.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -ex
+wget https://github.com/google/protobuf/releases/download/v2.4.1/protobuf-2.4.1.tar.gz
+tar -xzvf protobuf-2.4.1.tar.gz
+cd protobuf-2.4.1 && ./configure --prefix=/usr && make && sudo make install
+
+
