a33a04cb7b16265a7c9d6678e3a01751cb7dccc6
Christian Thunström <christian@qxx.se>
2018-01-20 22:54:09 +0000

    no tagging and version

--- ci/git-tag.sh
+++ ci/git-tag.sh
@@ -2,7 +2,6 @@
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
 
-
 JARFILE=$(find target/ -name '*.jar' -exec basename {} \;)
 VERSION=v`echo $JARFILE | sed 's/.jar//' | sed 's/[[:alpha:]|(|[:space:]|-]//g'`
 
@@ -10,7 +9,7 @@ echo "Version :: $VERSION"
 echo "TRAVIS_BRANCH :: $TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST :: $TRAVIS_PULL_REQUEST"
 
-if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ "$TRAVIS_BRANCH" == "master" ]]; then
+if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ "$TRAVIS_BRANCH" == "master" ]];then
    git tag $VERSION -a -m "Version $VERSION"
    git push -q https://$GH_TOKEN@github.com/krussan/protodb --tags
 fi
