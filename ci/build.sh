08378d0b5255df57d8b547c6cdaf3f887df2af39
Christian Thunström <christian@qxx.se>
2018-01-20 19:51:33 +0000

    It's all about the quotes

--- ci/build.sh
+++ ci/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo Initiating build ...
 
-if [[ $TRAVIS_PULL_REQUEST == “false” ]] && [[ $TRAVIS_BRANCH == “master” ]]; then
+if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ "$TRAVIS_BRANCH" == "master" ]];then
    echo Packaging new release ...
    mvn clean package -B
-elif [[ $TRAVIS_PULL_REQUEST == “true” ]] && [[ $TRAVIS_BRANCH == “master” ]]; then
+elif [[ "$TRAVIS_PULL_REQUEST" == "true" ]] && [[ "$TRAVIS_BRANCH" == "master" ]];then
    echo Pull request build.
    echo Checking that the resulting tag does not exist
 else 
