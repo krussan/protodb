01797d71990c7d823c3bc391d63b5b7d32a50ed0
Christian Thunström <christian@qxx.se>
2018-01-20 19:14:36 +0000

    set library path

--- ci/build.sh
+++ ci/build.sh
@@ -4,7 +4,10 @@ echo Initiating build ...
 if [[ $TRAVIS_PULL_REQUEST == “false” ]] && [[ $TRAVIS_BRANCH == “master” ]]; then
    echo Packaging new release ...
    mvn clean package -B
-else
+elif [[ $TRAVIS_PULL_REQUEST == “true” ]] && [[ $TRAVIS_BRANCH == “master” ]]; then
+   echo Pull request build.
+   echo Checking that the resulting tag does not exist
+else 
    echo Running test ...
    mvn test -B
 fi
