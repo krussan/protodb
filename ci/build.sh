184837e5e3e02d2c8927eb218ed0041a1e3ef839
Christian Thunström <christian@qxx.se>
2018-01-20 18:51:25 +0000

    Fixed settings and debug build script

--- ci/build.sh
+++ ci/build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+echo Initiating build ...
+
 if [[ $TRAVIS_PULL_REQUEST == “false” ]] && [[ $TRAVIS_BRANCH == “master” ]]; then
+   echo Packaging new release ...
    mvn clean package -B
 else
+   echo Running test ...
    mvn test -B
 fi
 
