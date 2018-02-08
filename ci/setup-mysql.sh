0cea7d17a1c883791f4c59641a6955ca583f41e8
Christian Thunström <christian@qxx.se>
2018-02-08 14:34:17 +0100

    Added setup script for mysql test db

--- ci/setup-mysql.sh
+++ ci/setup-mysql.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
-MYSQL_SCRIPT = "LOAD DATA INFILE '/c/media/tmp/csv/enumone.csv' INTO TABLE EnumOne COLUMNS TERMINATED BY ';' LINES TERMINATED BY '\n' IGNORE 1 LINES"
+DBUSER=$1
+DBPWD=$2
+DB=$3
+SCRIPT_DIR=$4
+
+#MYSQL_SCRIPT = "LOAD DATA INFILE '/c/media/tmp/csv/enumone.csv' INTO TABLE EnumOne COLUMNS TERMINATED BY ';' LINES TERMINATED BY '\n' IGNORE 1 LINES"
+
+for f in $SCRIPT_DIR/*.sql; do 
+  echo "Processing $f file.."; 
+  mysql -u $DBUSER --password=$DBPWD -D $DB < $f
+done
+
+for f in $SCRIPT_DIR/*.csv; do 
+  echo "Importing $f file.."; 
+  filename=$(basename "$f")
+  NAME="${filename%.*}"
+
+  echo "LOAD DATA INFILE '$f' INTO TABLE $NAME COLUMNS TERMINATED BY ';' LINES TERMINATED BY '\n' IGNORE 1 LINES"  > mysql_temp.sql
+  mysql -u $DBUSER --password=$DBPWD -D $DB < mysql_temp.sql
+done
+
+
+
