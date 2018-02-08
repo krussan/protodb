7beecee6d2ddec7bf7fb913c64ccf42c047fb316
Christian Thunström <christian@qxx.se>
2018-02-08 23:20:29 +0100

    Updated travis with mysql initialization

---
+++ ci/mysql-init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mysql -u root -e "CREATE USER protodb IDENTIFIED BY 'protodb'"
+mysql -u root -e "CREATE DATABASE protodb"
+mysql -u root -e "CREATE DATABASE protodb_select"
+mysql -u root -e "GRANT ALL PRIVILEGES ON protodb.* TO protodb"
+mysql -u root -e "GRANT ALL PRIVILEGES ON protodb_select.* TO protodb"
+mysql -u root -e "GRANT FILE ON *.* TO protodb"
