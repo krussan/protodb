c5d10abe5c68cfe98afa989c5d5f29295de0974e
Christian Thunström <christian@qxx.se>
2018-02-08 23:13:57 +0100

    Finalized script for mysql setup

--- ci/setup-mysql.sh
+++ ci/setup-mysql.sh
@@ -8,16 +8,23 @@ SCRIPT_DIR=$4
 
 for f in $SCRIPT_DIR/*.sql; do 
   echo "Processing $f file.."; 
+  FULLPATH=$(realpath $f | sed "s/\//\\\\\//gi")
+  FILENAME=$(basename "$f")
+  NAME="${FILENAME%.*}"
+
+  echo "DROP TABLE $NAME" | mysql -u $DBUSER --password=$DBPWD -D $DB
   mysql -u $DBUSER --password=$DBPWD -D $DB < $f
 done
 
 for f in $SCRIPT_DIR/*.csv; do 
   echo "Importing $f file.."; 
-  filename=$(basename "$f")
-  NAME="${filename%.*}"
+  FULLPATH=$(realpath $f | sed "s/\//\\\\\//gi")
+  FILENAME=$(basename "$f")
+  NAME="${FILENAME%.*}"
 
-  echo "LOAD DATA INFILE '$f' INTO TABLE $NAME COLUMNS TERMINATED BY ';' LINES TERMINATED BY '\n' IGNORE 1 LINES"  > mysql_temp.sql
-  mysql -u $DBUSER --password=$DBPWD -D $DB < mysql_temp.sql
+   sed "s/__FILE__/$FULLPATH/gi" $SCRIPT_DIR/import.script | \
+      sed "s/__TABLE__/$NAME/gi" | \
+      mysql -u $DBUSER --password=$DBPWD -D $DB 
 done
 
 
