96a0570b50adafbad2ebb937479f8128e56c1eec
Christian Thunström <christian@qxx.se>
2018-02-09 21:22:41 +0000

    Added host to mysql script

--- ci/mysql-setup.sh
+++ ci/mysql-setup.sh
@@ -3,6 +3,11 @@ DBUSER=$1
 DBPWD=$2
 DB=$3
 SCRIPT_DIR=$4
+HOST=$5
+
+if [ -z "$HOST" ]; then
+  HOST=localhost
+fi
 
 #MYSQL_SCRIPT = "LOAD DATA INFILE '/c/media/tmp/csv/enumone.csv' INTO TABLE EnumOne COLUMNS TERMINATED BY ';' LINES TERMINATED BY '\n' IGNORE 1 LINES"
 
@@ -13,8 +18,8 @@ for f in $SCRIPT_DIR/*.sql; do
   FULLPATH=$(cd "$DIRNAME"; pwd)/$FILENAME
   NAME="${FILENAME%.*}"
 
-  mysql -u $DBUSER --password=$DBPWD -D $DB -e "DROP TABLE IF EXISTS $NAME"
-  mysql -u $DBUSER --password=$DBPWD -D $DB < $FULLPATH
+  mysql -h $HOST -u $DBUSER --password=$DBPWD -D $DB -e "DROP TABLE IF EXISTS $NAME"
+  mysql -h $HOST -u $DBUSER --password=$DBPWD -D $DB < $FULLPATH
 done
 
 for f in $SCRIPT_DIR/*.csv; do 
@@ -27,7 +32,7 @@ for f in $SCRIPT_DIR/*.csv; do
 
    sed "s/__FILE__/$FULLPATH/gi" $SCRIPT_DIR/import.script | \
       sed "s/__TABLE__/$NAME/gi" | \
-      mysql -u $DBUSER --password=$DBPWD -D $DB --local-infile=1
+      mysql -h $HOST -u $DBUSER --password=$DBPWD -D $DB --local-infile=1
 done
 
 
