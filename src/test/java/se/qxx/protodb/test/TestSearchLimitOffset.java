e5cf9a93530386f8243edf707abf741682683088
Christian Thunström <christian@qxx.se>
2018-03-18 15:31:00 +0000

    Added test for order by search

--- src/test/java/se/qxx/protodb/test/TestSearchLimitOffset.java
+++ src/test/java/se/qxx/protodb/test/TestSearchLimitOffset.java
@@ -18,6 +18,7 @@ import se.qxx.protodb.JoinResult;
 import se.qxx.protodb.ProtoDB;
 import se.qxx.protodb.ProtoDBFactory;
 import se.qxx.protodb.ProtoDBScanner;
+import se.qxx.protodb.ProtoDBSort;
 import se.qxx.protodb.Searcher;
 import se.qxx.protodb.exceptions.DatabaseNotSupportedException;
 import se.qxx.protodb.exceptions.IDFieldNotFoundException;
@@ -142,6 +143,47 @@ public class TestSearchLimitOffset extends TestBase {
 			e.printStackTrace();
 			fail(e.getMessage());
 		}
+	}
+
+	@Test
+	public void TestLimitOffsetOrderBy() throws IDFieldNotFoundException {
+		try {
+			
+		    db.setupDatabase(TestDomain.RepObjectOne.newBuilder());
+			
+		    for (int i=1;i<50;i++) {
+				RepObjectOne o1 = RepObjectOne.newBuilder()
+						.setID(i)
+						.setHappycamper(i)
+						.build();
+				
+				db.save(o1);
+		    }
+
+			List<TestDomain.RepObjectOne> result =
+				db.search(
+					TestDomain.RepObjectOne.getDefaultInstance(), 
+					"", 
+					"%", 
+					ProtoDBSearchOperator.Like,
+					10, 
+					0,
+					"happycamper",
+					ProtoDBSort.Desc);
+			
+			// we should get 10 results
+			assertEquals(10, result.size());
+			
+			// the first should be number 1
+			assertEquals(49, result.get(0).getHappycamper());
+			
+			// the last should be number 10
+			assertEquals(40, result.get(9).getHappycamper());
+
+		} catch (SQLException | ClassNotFoundException | SearchFieldNotFoundException | ProtoDBParserException  e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}		
 	}
 
 }
