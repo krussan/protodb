8326c8976e93f656069981fc52fb424870e79457
Christian Thunström <christian@qxx.se>
2022-01-06 17:03:15 +0100

    fix credentials

--- ci/git-creds.sh
+++ ci/git-creds.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git config credential.helper "store --file=.git/credentials"
-echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+echo "https://krussan:${GH_TOKEN}@github.com" > .git/credentials
