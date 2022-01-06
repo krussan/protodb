d85dd2ff1cf1287615b61c258770a0ac204891e7
krussan <christian@qxx.se>
2022-01-06 16:56:49 +0100

    with credential helper (#61)

--- ci/mvn-install.sh
+++ ci/mvn-install.sh
@@ -5,8 +5,9 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ "$TRAVIS_BRANCH" == "master" ]]
   echo "Installing to local maven repo...."
   MVN_REPO=$GIT_BASE/maven-repo
 
-  git clone git@github.com:krussan/maven-repo $MVN_REPO
-  
+  #git clone git@github.com:krussan/maven-repo $MVN_REPO
+  git clone https://github.com/krussan/maven-repo $MVN_REPO
+
   cd $TRAVIS_BUILD_DIR/target
   $MVN_REPO/mvn-install.sh ../pom.xml $JARFILE
     
